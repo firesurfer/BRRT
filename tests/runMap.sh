f95259ffba30db4f9a0e3e5f667a03bb2061b6e6
Lennart Nachtigall <firesurfer127@gmail.com>
2017-04-16 18:25:55 +0200

    updated tests

--- tests/runMap.sh
+++ tests/runMap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+mkdir -p results
 for i in `seq 1 1`;
 do
 
-mono BRRT.exe MAP_roundedFreeForRobot.png 1000,1247,90 2356,420,0 ResultMap$i.png 
+mono ../BRRT/bin/Debug/BRRT.exe MAP_roundedFreeForRobot.png 1000,1247,90 2356,420,0 results/ResultMap$i.png 
 
 done
